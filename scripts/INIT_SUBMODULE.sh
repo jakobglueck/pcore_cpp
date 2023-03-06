a09dee15415dea6fbbd3c08bc2044cd8268bf1cc
jakob glueck <jakob.glueck@preventicus.com>
2023-03-06 15:14:20 +0100

    update gitignore

--- scripts/INIT_SUBMODULE.sh
+++ scripts/INIT_SUBMODULE.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ..
-git submodule add https://github.com/protocolbuffers/protobuf.git external/google-protobuf
+git submodule add --force https://github.com/protocolbuffers/protobuf.git external/google-protobuf
 cd external/google-protobuf
 git checkout v21.9
 git submodule update --init --recursive
